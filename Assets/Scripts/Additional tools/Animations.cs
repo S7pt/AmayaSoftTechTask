3807e45e3a6f6ae9f894594c0198cfc5217d0040
S7pt <chaos160702@gmail.com>
2021-08-17 19:44:07 +0300

    Slight refactoring

--- Assets/Scripts/Additional tools/Animations.cs
+++ Assets/Scripts/Additional tools/Animations.cs
@@ -7,31 +7,31 @@ public class Animations
     private static void Bounce(Transform _object, float startingValue, float maximumValue, float endValue,float growDuration,
         float shrinkDuration)
     {
-        Sequence _bounceSequence = DOTween.Sequence();
-        _bounceSequence.Append(_object.DOScale(startingValue, 0f));
-        _bounceSequence.Append(_object.DOScale(maximumValue, growDuration));
-        _bounceSequence.Append(_object.DOScale(endValue, shrinkDuration));
+        Sequence bounceSequence = DOTween.Sequence();
+        bounceSequence.Append(_object.DOScale(startingValue, 0f));
+        bounceSequence.Append(_object.DOScale(maximumValue, growDuration));
+        bounceSequence.Append(_object.DOScale(endValue, shrinkDuration));
     }
 
     public static void EaseInBounceInCell(Transform _object)
     {
-        Sequence _easeInBounceSequence = DOTween.Sequence();
-        _easeInBounceSequence.Append(_object.DOLocalMoveY(4, 0.1f));
-        _easeInBounceSequence.Append(_object.DOLocalMoveY(-4, 0.1f));
-        _easeInBounceSequence.Append(_object.DOLocalMoveY(9, 0.1f));
-        _easeInBounceSequence.Append(_object.DOLocalMoveY(-9, 0.1f));
-        _easeInBounceSequence.Append(_object.DOLocalMoveY(13, 0.1f));
-        _easeInBounceSequence.Append(_object.DOLocalMoveY(0, 0.1f));
+        Sequence easeInBounceSequence = DOTween.Sequence();
+        easeInBounceSequence.Append(_object.DOLocalMoveY(4, 0.1f));
+        easeInBounceSequence.Append(_object.DOLocalMoveY(-4, 0.1f));
+        easeInBounceSequence.Append(_object.DOLocalMoveY(9, 0.1f));
+        easeInBounceSequence.Append(_object.DOLocalMoveY(-9, 0.1f));
+        easeInBounceSequence.Append(_object.DOLocalMoveY(13, 0.1f));
+        easeInBounceSequence.Append(_object.DOLocalMoveY(0, 0.1f));
     }
 
-    public static void BounceAsASlot(Transform _object, float growDuration, float shrinkDuration)
+    public static void BounceAsASlot(Transform slot, float growDuration, float shrinkDuration)
     {
-        Bounce(_object, 0, 1.2f, 1, growDuration, shrinkDuration);
+        Bounce(slot, 0, 1.2f, 1, growDuration, shrinkDuration);
     }
 
-    public static void BounceAsASprite(Transform _object, float growDuration, float shrinkDuration)
+    public static void BounceAsASprite(Transform cell, float growDuration, float shrinkDuration)
     {
-        Bounce(_object, 1,1.2f,1, growDuration, shrinkDuration);
+        Bounce(cell, 1,1.2f,1, growDuration, shrinkDuration);
     }
 
     //I couldn't find the way to avoid code reusage, so that was the only way
